0cf538d1b6861b3cf8765bd4446ad92b37f0fc51
uwazac <gstephens@StephensFamilyMacBook.home>
2015-04-14 22:47:10 -0400

    updated

--- PA1_template.R
+++ PA1_template.R
@@ -1,6 +1,21 @@
+library("lattice")
+library("dplyr")
+
+activity<-read.csv("activity.csv")
+dates<-as.POSIXlt(strftime(activity$date, format="", usetz=F))
+dates<-as.Date(dates, format="%d%m%Y")
+activity$date<-dates
+month_dates<-months(dates)
+
+steps_perDay<-distinct(activity, steps)
+
 steps_perDayTotal<-summarise(group_by(steps_perDay, date), sum(steps))
+
 bad<-is.na(steps_perDayTotal$"sum(steps)")
 good_steps<-steps_perDayTotal$"sum(steps)"[!bad]
+
+print(histogram(~`sum(steps)`, data=good_steps_perDayTotal, xlab="Total Steps", scales=list(alternating='2', cex=c(0.9,1)), nint=52, auto.key=T))
+
 mean(good_steps)
 median(good_steps)
 bad_rows<-lapply(steps_perDayTotal$"sum(steps)", function(x) is.na(x))
